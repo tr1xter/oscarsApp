fa31e2c2cdb1e822d4762292c6a82045e9143750
tr1xter <aleksi.waheri@hotmail.com>
2014-02-28 23:18:47 +0200

    chart updates

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-var scoreboardApp = angular.module('scoreboardApp', []);
+var scoreboardApp = angular.module('scoreboardApp', ["highcharts-ng"]);
  
 scoreboardApp.controller('NomineesListCtrl', function ($scope, $http) {
 	$http.get('nominees/nominees.json').success(function(data) {
@@ -16,8 +16,95 @@ scoreboardApp.controller('NomineesVisibilityCtrl', function ($scope) {
 		$('.categoryContainer').css('width','40px');
 		$('#' +nominee +'div').css('width','250px');
 		$('#' +nominee).css('display','none');
-		
-		$('#chartBackgroundDiv').css('background-image', 'url(img/nomineesPics/'+nominee+'BG.jpg)');
-		$('#chartBackgroundDiv').hide().fadeIn(2000);
     }
+});
+
+scoreboardApp.controller('ChartCtrl', function ($scope) {
+        //var seriesArray = $scope.chart.series;
+		
+	$scope.chartTypes = [
+        {"id": "bar", "title": "Bar"}
+    ];
+
+    $scope.chartSeries = [
+        {"name": "Aleksi", "data": [12]},
+        {"name": "Antti", "data": [13]},
+        {"name": "Julius", "data": [15]},
+        {"name": "Mikko", "data": [11]},
+        {"name": "Jukka", "data": [18]},
+        {"name": "Pertti", "data": [14]},
+        {"name": "Esko", "data": [15]},
+        {"name": "Jaakko", "data": [16]},
+        {"name": "Ismo", "data": [12]},
+        {"name": "Kalle", "data": [17]}
+    ];
+
+    $scope.chartConfig = {
+        options: {
+            chart: {
+                type: 'bar',
+				backgroundColor:'rgba(255, 255, 255, 0.8)'
+            },
+            plotOptions: {
+                series: {
+                    stacking: ''
+                }
+            }
+        },
+		xAxis: {
+			title: {
+				text: ''
+			},
+			//categories: $scope.chartSeries.series
+			categories: ['Aleksi','Antti','Julius']
+			//categories: scope.$eval(name)
+		},
+		yAxis: {
+			title: {
+				text: ''
+			}
+		},
+        series: $scope.chartSeries,
+        title: {
+            text: 'Score'
+        },
+        credits: {
+            enabled: false
+        },
+		exporting: {
+			enabled: false
+		},
+		legend: {
+			enabled: false
+		},
+        loading: false
+    }
+		
+	/*
+    var score = ['32','45'];
+	
+    for (var i = 0; i < data.length; i++) {
+        score.push(data[i].Score);   
+    }
+
+    var name = ['Orange','Apple'];
+	
+    for (var i = 0; i < data.length; i++) {
+        name.push(data[i].Name);
+    }
+
+    $scope.renderChart = {
+        chart: {
+            type: 'bar'
+        },
+        xAxis: {
+            categories: name
+        },
+        series: [{
+            data: score
+        }],
+        legend: {
+            enabled: false
+        }
+    }*/
 });
